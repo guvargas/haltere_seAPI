4cb87d3f921e8a2ca098311f43cc9d99272edb72
GUSTAVO VARGAS DE ANDRADE <gupontoandrade00@gmail.com>
2021-12-16 10:42:49 -0300

    minor changes

--- bancodedados/commoncomands.sql
+++ bancodedados/commoncomands.sql
@@ -1,3 +1,3 @@
-select * from acoes.post;
-delete from acoes.post;
+select * from aluno;
+delete from aluno;
 update aluno set nome = 'a', cpf = 'a', senha ='a', cargaHoraria='2', dataNascimento='$5', peso=3, altura=4,observacoes='$8' where idaluno = 
